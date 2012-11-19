7cb6e59d7c86f034c2225deb96281f7690f7b8ed
Ryan Maxwell <ryan@cactuslab.com>
2012-11-20 12:58:38 +1300

    Empty file to compile

---
+++ SnaprKitFramework.h
@@ -0,0 +1,15 @@
+//
+//  SnaprKitFramework.h
+//  SnaprCocoapods
+//
+//  Created by Ryan Maxwell on 20/11/12.
+//  Copyright (c) 2012 Snapr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SnaprKitFramework : NSObject
+
++ (NSString *)version;
+
+@end
---
+++ SnaprKitFramework.m
@@ -0,0 +1,17 @@
+//
+//  SnaprKitFramework.m
+//  SnaprCocoapods
+//
+//  Created by Ryan Maxwell on 20/11/12.
+//  Copyright (c) 2012 Snapr. All rights reserved.
+//
+
+#import "SnaprKitFramework.h"
+
+@implementation SnaprKitFramework
+
++ (NSString *)version {
+    return @"0.1";
+}
+
+@end
